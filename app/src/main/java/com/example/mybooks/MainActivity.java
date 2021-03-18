07d9f863b6b01ea5728f2cafc56f0dc76d2c4861
markusmeresma <markus.meresma@gmail.com>
2021-03-18 13:33:38 +0200

    URL is broken

--- app/src/main/java/com/example/mybooks/MainActivity.java
+++ app/src/main/java/com/example/mybooks/MainActivity.java
@@ -4,18 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String url = "https://www.googleapis.com/books/v1/volumes?q=flowers";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new SearchTask().execute(url);
+        EditText searchInput = (EditText) findViewById(R.id.searchInput);
+        Button searchButton = (Button) findViewById(R.id.searchButton);
+
+        // A variable to save user input
+        String queryParams = searchInput.getText().toString();
+        Log.i(getResources().getString(R.string.app_name), "User input " + queryParams);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SearchTask searchTask = new SearchTask();
+                // URL is broken
+                String requestURL = searchTask.createURLString(queryParams);
+                Log.i(getResources().getString(R.string.app_name), "URL " + requestURL);
+                new SearchTask().execute(requestURL);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/mybooks/SearchTask.java
+++ app/src/main/java/com/example/mybooks/SearchTask.java
@@ -8,9 +8,26 @@ public class SearchTask extends AsyncTask<String, Void, String> {
     private Utils utils = new Utils();
     private static final String log_tag = SearchTask.class.getSimpleName();
 
+    public String createURLString (String requestParameters)
+    {
+        String stringURL;
+
+        // Something is broken here - try using uri in utils class to build up the url instead of this approach
+        // Build URL string with search parameters given by the user
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(R.string.GOOGLE_BOOKS_API + requestParameters);
+
+        stringURL = stringBuilder.toString();
+
+        Log.i(log_tag, "stringURL = " + stringURL);
+
+        return stringURL;
+    }
 
     @Override
     protected String doInBackground(String... urls) {
+
+        // This method is currently used for debugging
 
         String requestedData;
 
